1fdcff8e6f0090731db41c99b7e5b3629642883e
Carlos Perez <caperez@blizzard.com>
2017-08-14 22:54:59 -0700

    trying new routes

---
+++ app/components/Category.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import {Router, Route} from 'react-router';
+
+class Category extends Component {
+    render() {
+        return (
+            <h1>Category Page Test</h1>
+        )
+    }
+}
+
+module.exports = Category;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,15 +1,15 @@
 var React = require("react");
-var router = require("react-router");
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { render } from 'react-dom';
 
 
 // require Main.js
 var Main = require("../components/Main");
+var Category = require("../components/Category");
 
 module.exports = (
     <Router>
-        <div>
-            <Route path="/" component={Main}/>
-        </div>
+        <Route path="/" component={Main}/>
+        {/* <Route path="category" component={Category}/> */}
     </Router>
-)
+);
