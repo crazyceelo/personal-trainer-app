fa32d3167662a84e2e1b9a05c011bb88aa096b5f
Carlos Perez <solracias@gmail.com>
2017-08-14 19:10:07 -0700

    server is set up

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Server configuration
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+app.use(express.static("public"));
+
+// MongoDB configuration
+mongoose.connect("mongodb://localhost/personal-trainer-app");
+var db = mongoose.connection;
+
+db.on("error", function(error){
+    console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function(){
+    console.log("Mongoose connection successful.");
+})
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+})
