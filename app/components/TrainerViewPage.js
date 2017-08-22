62ecc851368d553e70543837516e72027ce6855c
Carlos Perez <solracias@gmail.com>
2017-08-21 18:55:20 -0700

    trainer view page done

--- app/components/TrainerViewPage.js
+++ app/components/TrainerViewPage.js
@@ -3,7 +3,49 @@ import React, { Component } from 'react';
 export default class TrainerViewPage extends Component {
     render() {
         return (
-            <h1 className="text-center">TrainerViewPage Page Test</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <br />
+                    </div>
+                </div>
+                <dl className="row">
+                    <dt className="col-sm-2">Full Name: </dt>
+                    <dd className="col-sm-9">asdfasdf</dd>
+              
+                    <dt className="col-sm-2">Specialization: </dt>
+                    <dd className="col-sm-9">asdfasdfasdfsa</dd>
+              
+                    <dt className="col-sm-2">Email: </dt>
+                    <dd className="col-sm-9">asdfasdfasdf</dd>
+                </dl>
+                <dl className="row">
+                    <div>
+                        <dt className="col-sm-2">Description: </dt>
+                    </div>
+                </dl>
+                <dl className="row">
+                    <div>
+                        <dd className="col-sm-12"> 
+                            test  test  test  test  test  test
+                            asdfasdfasdfasdfasdf asdfasdfasdfasdfasdfasdf
+                            asdfasdf asdf asdf asdf asd fa ds fasdf asd faasdf
+                            sdfasd asdf asdf asdf sfsd asd fasdf asd fas dfasdfd
+                            qwfe gwerwerg f fgfgsfgsfdg erg erg ertgreger hrheth
+                            test  test  test  test  test  test
+                            asdfasdfasdfasdfasdf asdfasdfasdfasdfasdfasdf
+                            asdfasdf asdf asdf asdf asd fa ds fasdf asd faasdf
+                            sdfasd asdf asdf asdf sfsd asd fasdf asd fas dfasdfd
+                            qwfe gwerwerg f fgfgsfgsfdg erg erg ertgreger hrheth
+                            test  test  test  test  test  test
+                            asdfasdfasdfasdfasdf asdfasdfasdfasdfasdfasdf
+                            asdfasdf asdf asdf asdf asd fa ds fasdf asd faasdf
+                            sdfasd asdf asdf asdf sfsd asd fasdf asd fas dfasdfd
+                            qwfe gwerwerg f fgfgsfgsfdg erg erg ertgreger hrheth
+                            </dd>
+                    </div>
+                </dl>
+            </div>
         )
     }
 }
