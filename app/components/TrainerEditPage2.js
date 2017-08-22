12240fc00c9854aa785078b70070715627e99bc9
Carlos Perez <solracias@gmail.com>
2017-08-21 18:19:58 -0700

    deleted trainer edit page 2. It wasn't needed

--- app/components/TrainerEditPage2.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class TrainerEditPage2 extends Component {
-    render() {
-        return (
-            <h1 className="text-center">Trainer Edit Page 2 test</h1>
-        )
-    }
-}
