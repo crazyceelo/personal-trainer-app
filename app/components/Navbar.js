15d6586b6836540306cc608b9bb8f4b68ee00d60
Carlos Perez <solracias@gmail.com>
2017-08-22 18:13:26 -0700

    fixed navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,25 +4,29 @@ export default class Navbar extends Component {
     render(){
         return (
             <div>
-                <nav className="navbar navbar-light bg-faded">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-1">
-                                <a className="navbar-brand" href="/">Home</a>
-                            </div>
-                            <div className="col-md-3 ">
-                                <a className="nav-link text-center" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Sign up</a>
-                            </div>
-                            <div className="col-md-4">
-                                <a className="nav-link text-center" href="#">Member Login</a>
-                            </div>
-                            <div className="col-md-4">
-                                <a className="nav-link text-center" href="#">Trainer Login</a>
-                            </div>
-                        </div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
                     </div>
                 </nav>
-                <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                {/* <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                   <div className="modal-dialog" role="document">
                     <div className="modal-content">
                       <div className="modal-header">
@@ -40,7 +44,7 @@ export default class Navbar extends Component {
                       </div>
                     </div>
                   </div>
-                </div>
+                </div> */}
             </div>
         )
     }
