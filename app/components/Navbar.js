50bd2dc97550b31d6de7e067bfc5a2c803981d94
Carlos Perez <solracias@gmail.com>
2017-09-01 15:06:11 -0700

    added html, body css

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -51,10 +51,10 @@ export default class Navbar extends Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item active">
+                        <li className="nav-item">
                         <a className="nav-link" href="#" data-toggle="modal" data-target="#signUpMemberModal">Sign up as member <span className="sr-only">(current)</span></a>
                         </li>
-                        <li className="nav-item active">
+                        <li className="nav-item">
                         <a className="nav-link" href="/#/trainer-edit-page">Sign up as trainer <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
