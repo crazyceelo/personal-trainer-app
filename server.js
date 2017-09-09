6d547f9bb174d5dfd7b51bcc2767e7d9405e4f11
Carlos Perez <solracias@gmail.com>
2017-09-08 21:37:14 -0700

    required cookie-parser

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var routes = require("./config/api-routes");
 var http = require ('http');
+var cookieParser = require("cookie-parser");
 
 // Server configuration
 var app = express();
@@ -12,6 +13,7 @@ var PORT = process.env.PORT || 3000;
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
